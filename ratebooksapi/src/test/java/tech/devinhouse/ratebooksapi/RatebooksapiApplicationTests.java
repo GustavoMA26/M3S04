027c2d34868544bcb04d7823dd89689a83776967
Gustavo <gmalmeida26@gmail.com>
2024-02-23 19:33:15 -0300

    database setup

---
+++ ratebooksapi/src/test/java/tech/devinhouse/ratebooksapi/RatebooksapiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.ratebooksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatebooksapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
